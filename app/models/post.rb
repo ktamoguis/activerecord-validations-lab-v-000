8307008f3e372c8389835f9c0a2459865af915bd
Karl Amoguis <ktamoguis@gmail.com>
2018-07-09 09:37:31 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,12 +7,12 @@ class Post < ActiveRecord::Base
 
   validate :clickbait?
 
-   CLICKBAIT_PATTERNS = ["Won't Believe", "Secret", "Guess"]
-#    /Won't Believe/i,
-#    /Secret/i,
-#    /Top [0-9]*/i,
-#    /Guess/i
-#  ]
+   CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
+  ]
 
   def clickbait?
     if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
